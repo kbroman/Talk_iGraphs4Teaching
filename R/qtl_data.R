2afda4fa1b0588ae280286408c03808ea1eaf1c6
Karl Broman <kbroman@gmail.com>
2014-11-05 11:15:57 -0600

    Rearrange panels in plot of QTL data

--- R/qtl_data.R
+++ R/qtl_data.R
@@ -5,8 +5,8 @@ source("my_geno_image.R")
 png(file="../Figs/qtl_data.png", width=1180, height=600, res=108,
     pointsize=14, bg="transparent")
 par(fg="white", col.axis="white", col.main="wheat", col.lab="#80daeb",
-    cex.main=1.5, cex.lab=1.5)
-layout(cbind(1,2,3), width=c(8,1,8))
+    cex.main=1.8, cex.lab=1.5)
+layout(cbind(1,2,3), width=c(8,8,1.4))
 
 data(hyper)
 hyper <- hyper[,order(hyper$pheno[,1])]
@@ -15,11 +15,16 @@ bg <- "transparent"
 blue <- "#0074D9"
 red <- "#FF4136"
 
+par(mar=c(5.1, 4.6, 4.1, 1.6))
+plot.map(hyper, main="")
+title(main="Genetic map", line=3)
+
+par(mar=c(5.1, 4.6, 4.1, 1.6))
 my.geno.image(hyper, alternate=TRUE, main="",
               thecolors=c(bg, blue, red))
 title(main="Genotypes", line=3)
 
-par(mar=c(5.1, 1.6, 4.1, 1.6))
+par(mar=c(5.1, 2.1, 4.1, 3.1))
 color <- c(rev(colorRampPalette(c("white", blue))(256)[-1]),
            colorRampPalette(c("white", red))(256))
 
@@ -28,9 +33,6 @@ image(1, 1:nind(hyper), t(hyper$pheno[,1,drop=FALSE]),
 title(main="Pheno", line=3)
 axis(side=2, at=seq(50, 250, by=50), label=rep("", 5))
 
-par(mar=c(5.1, 4.1, 4.1, 2.1))
-plot.map(hyper, main="")
-title(main="Genetic map", line=3)
 
 
 
