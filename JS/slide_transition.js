014d6f24a2286f5091c02c8d1a08746fd5a6eece
Karl Broman <kbroman@gmail.com>
2014-11-10 11:01:16 -0600

    Revise slide_transition.js for placement of D3 slide

--- JS/slide_transition.js
+++ JS/slide_transition.js
@@ -1,18 +1,20 @@
 current_slide = null;
 
 slide_transition = function(slidenumber) {
-  console.log("transition " + current_slide + " -> " + slidenumber);
+    console.log("transition " + current_slide + " -> " + slidenumber);
 
-  if(slidenumber == current_slide) return;
-  current_slide = slidenumber;
+    if(slidenumber == current_slide) return;
+    current_slide = slidenumber;
 
 
-  if(slidenumber==30) {
-    d3.select("p#dotenter").transition()
-                           .style("opacity", 1)
-                           .delay(20000)
-                           .duration(5000)
-                           .ease("linear");
+    if(slidenumber==35) {
+        console.log("entered D3 slide")
+
+        d3.select("p#dotenter").transition()
+                               .style("opacity", 1)
+                               .delay(20000)
+                               .duration(5000)
+                               .ease("linear");
 
   }
 }
